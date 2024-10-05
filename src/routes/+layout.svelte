d4eff0e3005c6e45ccc112c9ae2dc2c5efef6c3b
instantRahmen <42784024+instantrahmen@users.noreply.github.com>
2024-10-05 06:07:54 +0000

    Create navbar and switch season selector to use normal links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,48 @@
 <script lang="ts">
 	import 'highlight.js/styles/github-dark.min.css';
 	import '../app.css';
-	import { ModeWatcher } from 'mode-watcher';
+	import { scale } from 'svelte/transition';
+
+	import { ModeWatcher, toggleMode, mode } from 'mode-watcher';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import { Button } from '$lib/components/ui/button';
+	import { quintOut, cubicOut } from 'svelte/easing';
+
+	let { children } = $props();
 </script>
 
 <ModeWatcher />
 
-<slot></slot>
+<header
+	class="m-4 flex flex-row items-center justify-start rounded-md border bg-card p-3 text-lg text-card-foreground shadow-md"
+>
+	<h1 class="flex items-center text-2xl font-thin">
+		<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" role="img">
+			<title>Anigiri</title>
+			<path
+				fill="currentColor"
+				d="m231.53 146.57l-55.85-95.91l-.11-.19a56 56 0 0 0-95.14 0l-.11.19l-55.85 95.91A56 56 0 0 0 72.09 232h111.82a56 56 0 0 0 47.62-85.43m-12.68 48.88A39.49 39.49 0 0 1 183.91 216H176v-48a16 16 0 0 0-16-16H96a16 16 0 0 0-16 16v48h-7.91a40 40 0 0 1-34-61.09a2 2 0 0 0 .11-.2l55.85-95.9a40 40 0 0 1 67.84 0l55.85 95.9a2 2 0 0 0 .11.2a39.5 39.5 0 0 1 1 40.54"
+			/>
+		</svg>
+		<span aria-hidden="true">nigiri</span>
+		<span class="sr-only">anigiri</span>
+	</h1>
+
+	<div class="ml-auto flex items-center">
+		<Button on:click={toggleMode} variant="outline" size="icon" class="relative">
+			{#if $mode === 'dark'}
+				<div class="absolute" transition:scale={{ duration: 500, delay: 0, opacity: 0.5, start: 0, easing: cubicOut }}>
+					<Sun class="h-[1.3em] w-[1.3em]" />
+				</div>
+			{:else}
+				<div class="absolute" transition:scale={{ duration: 500, delay: 0, opacity: 0.5, start: 0, easing: cubicOut }}>
+					<Moon class="h-[1.3em] w-[1.3em]" />
+				</div>
+			{/if}
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+	</div>
+</header>
+
+{@render children()}
--- src/routes/seasons/[year]/[season]/+page.svelte
+++ src/routes/seasons/[year]/[season]/+page.svelte
@@ -1,37 +1,55 @@
 <script lang="ts">
 	import * as Tabs from '$lib/components/ui/tabs';
 	import AnimeGrid from '$lib/components/modules/anime-grid/AnimeGrid.svelte';
-	import { validSeasons } from '$lib/utils';
+	import { cn, validSeasons } from '$lib/utils';
 	import { goto } from '$app/navigation';
 	import { ArrowLeft, ArrowRight } from 'lucide-svelte';
 	import { Button } from '$lib/components/ui/button';
 	let { data } = $props();
-
-	let localSeason = $state(data.season);
-	let localYear = $state(data.year);
-	$effect(() => {
-		// navigate new season
-		goto(`/seasons/${localYear}/${localSeason}`);
-	});
 </script>
 
-<!-- {localSeason} -->
 <div class="flex flex-row justify-end gap-4">
 	<h2 class="flex-1 self-center justify-self-start align-middle">{data.season} {data.year} anime</h2>
 	<div
-		class="flex items-center justify-center gap-2 rounded border bg-card text-sm font-semibold tracking-wide text-muted-foreground shadow-md"
+		class="flex items-center justify-center gap-2 rounded-md border bg-card text-sm font-semibold tracking-wide text-muted-foreground shadow-md"
 	>
-		<Button variant="ghost" size="icon" on:click={() => (localYear = localYear - 1)}><ArrowLeft /></Button>
-		<span>{localYear}</span>
-		<Button variant="ghost" size="icon" on:click={() => (localYear = localYear + 1)}><ArrowRight /></Button>
+		<Button variant="ghost" size="icon" href={`/seasons/${data.year - 1}/${data.season}`}><ArrowLeft /></Button>
+		<span>{data.year}</span>
+		<Button variant="ghost" size="icon" href={`/seasons/${data.year + 1}/${data.season}`}><ArrowRight /></Button>
 	</div>
-	<Tabs.Root bind:value={localSeason}>
+	<nav>
+		<ol
+			class={cn(
+				'flex h-10 flex-row items-center justify-center gap-1 rounded-md bg-muted p-1 text-muted-foreground',
+				'mx-4 self-end border bg-card shadow-md'
+			)}
+		>
+			{#each validSeasons as season}
+				<li>
+					<Button
+						variant="ghost"
+						href={`/seasons/${data.year}/${season}`}
+						data-state={data.season === season ? 'active' : 'inactive'}
+						class={cn(
+							'inline-flex h-min items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5',
+							'text-sm font-medium ring-offset-background transition-all',
+							'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+							'disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm'
+						)}
+					>
+						{season}
+					</Button>
+				</li>
+			{/each}
+		</ol>
+	</nav>
+	<!-- <Tabs.Root bind:value={localSeason}>
 		<Tabs.List class="mx-4 self-end border bg-card shadow-md">
 			{#each validSeasons as season}
 				<Tabs.Trigger value={season}>{season}</Tabs.Trigger>
 			{/each}
 		</Tabs.List>
-	</Tabs.Root>
+	</Tabs.Root> -->
 </div>
 
 <AnimeGrid anime={data.anime.data} />
